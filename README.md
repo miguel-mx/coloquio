2456745f76f98b53792448679ab80db84191a738
Centro de Ciencias Matemáticas <ccmunam@CCMs-MacBook-Pro.local>
2016-04-26 11:27:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Módulo de Seminarios
+====================
+
+Módulo para Drupal 8 Crea bloque con el coloquio de la semana del CCM
---
+++ src/Plugin/Block/ColoquioBlock.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\coloquio\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'ColoquioBlock' block.
+ *
+ * @Block(
+ *  id = "coloquio_block",
+ *  admin_label = @Translation("Coloquio block"),
+ * )
+ */
+class ColoquioBlock extends BlockBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+
+    // Return: Render array
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "132.248.196.44/seminarios-CCM/web/app.php/evento/coloquio-semana.xml");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/xml'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+    $data = new \SimpleXMLElement($response);
+
+    $coloquio = (array)$data;
+
+    return [
+        '#theme' => 'coloquio_ccm',   // Defined in .module
+        '#coloquio' => $coloquio,   // twig paramenters
+        '#attached' => [
+            'library' => [
+                'seminarios/seminarios-styles', //include our custom library for this response
+            ]
+        ]
+    ];
+
+
+  }
+}
